e90b56607b8bc33a11a8b10b087ca0de240c8ba0
Freya Gentz <zegentzy@protonmail.com>
2019-11-30 22:08:37 -0700

    Cargo fmt
    Signed-off-by: Freya Gentz <zegentzy@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -21,23 +21,30 @@ lazy_static! {
     pub static ref XFT: Result<x11_dl::xft::Xft, OpenError> = x11_dl::xft::Xft::open();
     pub static ref XT: Result<x11_dl::xt::Xt, OpenError> = x11_dl::xt::Xt::open();
     pub static ref XMU: Result<x11_dl::xmu::Xmu, OpenError> = x11_dl::xmu::Xmu::open();
-    pub static ref XRENDER: Result<x11_dl::xrender::Xrender, OpenError> = x11_dl::xrender::Xrender::open();
-    pub static ref XCURSOR: Result<x11_dl::xcursor::Xcursor, OpenError> = x11_dl::xcursor::Xcursor::open();
+    pub static ref XRENDER: Result<x11_dl::xrender::Xrender, OpenError> =
+        x11_dl::xrender::Xrender::open();
+    pub static ref XCURSOR: Result<x11_dl::xcursor::Xcursor, OpenError> =
+        x11_dl::xcursor::Xcursor::open();
     pub static ref GLX: Result<x11_dl::glx::Glx, OpenError> = x11_dl::glx::Glx::open();
-
-    pub static ref XINPUT: Result<x11_dl::xinput::XInput, OpenError> = x11_dl::xinput::XInput::open();
-    pub static ref XINPUT2: Result<x11_dl::xinput2::XInput2, OpenError> = x11_dl::xinput2::XInput2::open();
-
-    pub static ref XRANDR_2_2_0: Result<x11_dl::xrandr::Xrandr_2_2_0, OpenError> = x11_dl::xrandr::Xrandr_2_2_0::open();
-    pub static ref XRANDR: Result<x11_dl::xrandr::Xrandr, OpenError> = x11_dl::xrandr::Xrandr::open();
-
-    pub static ref XF86VMODE: Result<x11_dl::xf86vmode::Xf86vmode, OpenError> = x11_dl::xf86vmode::Xf86vmode::open();
-    pub static ref XTEST_XF86VMODE: Result<x11_dl::xtest::Xf86vmode, OpenError> = x11_dl::xtest::Xf86vmode::open();
-    pub static ref XRECORD_XF86VMODE: Result<x11_dl::xrecord::Xf86vmode, OpenError> = x11_dl::xrecord::Xf86vmode::open();
-
-    pub static ref XINERAMA_XLIB: Result<x11_dl::xinerama::Xlib, OpenError> = x11_dl::xinerama::Xlib::open();
+    pub static ref XINPUT: Result<x11_dl::xinput::XInput, OpenError> =
+        x11_dl::xinput::XInput::open();
+    pub static ref XINPUT2: Result<x11_dl::xinput2::XInput2, OpenError> =
+        x11_dl::xinput2::XInput2::open();
+    pub static ref XRANDR_2_2_0: Result<x11_dl::xrandr::Xrandr_2_2_0, OpenError> =
+        x11_dl::xrandr::Xrandr_2_2_0::open();
+    pub static ref XRANDR: Result<x11_dl::xrandr::Xrandr, OpenError> =
+        x11_dl::xrandr::Xrandr::open();
+    pub static ref XF86VMODE: Result<x11_dl::xf86vmode::Xf86vmode, OpenError> =
+        x11_dl::xf86vmode::Xf86vmode::open();
+    pub static ref XTEST_XF86VMODE: Result<x11_dl::xtest::Xf86vmode, OpenError> =
+        x11_dl::xtest::Xf86vmode::open();
+    pub static ref XRECORD_XF86VMODE: Result<x11_dl::xrecord::Xf86vmode, OpenError> =
+        x11_dl::xrecord::Xf86vmode::open();
+    pub static ref XINERAMA_XLIB: Result<x11_dl::xinerama::Xlib, OpenError> =
+        x11_dl::xinerama::Xlib::open();
     pub static ref XLIB: Result<x11_dl::xlib::Xlib, OpenError> = x11_dl::xlib::Xlib::open();
-    pub static ref XLIB_XCB: Result<x11_dl::xlib_xcb::Xlib_xcb, OpenError> = x11_dl::xlib_xcb::Xlib_xcb::open();
+    pub static ref XLIB_XCB: Result<x11_dl::xlib_xcb::Xlib_xcb, OpenError> =
+        x11_dl::xlib_xcb::Xlib_xcb::open();
 }
 
 #[macro_export]
